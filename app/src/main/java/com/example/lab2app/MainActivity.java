a42a375ccdf5cf8749a8ab7e84fc8bb2eae2626e
JoaquinManchenoP <manchenojj@g.cofc.edu>
2021-02-23 19:17:34 -0500

    Lab 2 part 2

--- app/src/main/java/com/example/lab2app/MainActivity.java
+++ app/src/main/java/com/example/lab2app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lab2app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +13,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final int REQUEST_CODE = 1;
 
     private Button button;
     private Intent i;
@@ -31,12 +37,33 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String s = editText.getText().toString();
                 i.putExtra("testString", s);
-                startActivity(i);
+//                startActivity(i);
+                startActivityForResult(i, REQUEST_CODE);
 
 
                 Log.i("root", "Clicked");
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            if(extras != null){
+
+                int imageId = extras.getInt("imageID");
+                Log.i("message", String.valueOf(imageId));
+                ConstraintLayout currentLayout = findViewById(R.id.main_layout);
+                currentLayout.setBackground(getDrawable(imageId));
+
+
+
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/lab2app/SecondActivity.java
+++ app/src/main/java/com/example/lab2app/SecondActivity.java
@@ -3,6 +3,8 @@ package com.example.lab2app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,22 +12,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    //Part 1
+
     TextView textView2;
+
+    ImageView moon;
+    ImageView waterfall;
+
+    private int imageID;
+
+    Intent i = getIntent();
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent i = getIntent();
-        String s = i.getExtras().getString("testString");
+////For part one
+//
+//        String s = i.getExtras().getString("testString");
+//
+//        textView2 = findViewById(R.id.textSecond);
+//
+//        textView2.setText(s);
 
-        textView2 = findViewById(R.id.textSecond);
 
-        textView2.setText(s);
+        moon = findViewById(R.id.moon);
+       moon.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               imageID = R.drawable.supermoon;
+
+               moon.setId(imageID);
+
+               finish();
+
+           }
+       });
+
+       waterfall = findViewById(R.id.waterfall);
+       waterfall.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               imageID = R.drawable.waterfall;
+
+               waterfall.setId((imageID));
+
+               finish();
+
+
+
+
+           }
+       });
+
+
 
 
     }
 
-
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        intent.putExtra("imageId ", imageID);
+        super.finish();
+    }
 }
